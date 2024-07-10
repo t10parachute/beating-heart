af6226497bcec3a55ac1d2f043389296f4cac052
t10parachute <t10parachute@users.noreply.github.com>
2024-07-10 12:39:44 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    basic.showIcon(IconNames.LeftTriangle)
+    basic.showIcon(IconNames.No)
+    basic.clearScreen()
+    basic.pause(500)
+})
